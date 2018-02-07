4f2174adda9da8edd39e751bdcb4b5c257fc0afc
Randall Helzerman <rahelzer@yahoo.com>
2018-02-06 20:49:33 -0500

    commit #2 : added ENABLE_FEATURE_3 set to false

--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 local config = {}
 
 
-# Example config file
+-- Example config file
 
 GENERAL = {
    ENABLE_FEATURE_1 = true,
-   ENABLE_FEATURE_2 = false
+   ENABLE_FEATURE_2 = false,
+   ENABLE_FEATURE_3 = false
 }
 
 
